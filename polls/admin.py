74876af70c81c8e39fa86bc394915a9328634e86
ahSOLO <junmenghan@gmail.com>
2020-11-10 21:08:05 -0800

    Completed Tutorial Part 7

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text'] #make pub date come before question text
+    fieldsets = [
+        (None,                  {'fields': ['question_text']}),
+        ('Date information',    {'fields': ['pub_date']}),
+    ]  
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+""" Anytime you need to change the admin options for a model, 
+create a model admin class and pass it as a second argument to admin.site.register()"""
+admin.site.register(Question, QuestionAdmin)
